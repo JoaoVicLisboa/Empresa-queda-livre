a280fe701bbbbb9b8bdea5a4a1f0784b1775a123
João Victor <107276396+JoaoVicLisboa@users.noreply.github.com>
2023-12-01 21:33:47 -0300

    Update Passageiro.java

--- Passageiro.java
+++ Passageiro.java
@@ -1,42 +1,48 @@
+// INTEGRANTES : João Victor de Souza, Danilo Ramos, Felipe Gurgel, Daniel Nunes
 public class Passageiro {
-    private String CPF;
-    private String Nome;
-    private String Endereco;
-    private String Telefone;
-    private int NumPassagem;
-    private int NumPoltrona;
-    private int NumVoo;
-    private String Horario;
+    private final String cpf;
+    private final String nome;
+    private final String endereco;
+    private final String telefone;
+    private int numPassagem;
+    private int numPoltrona;
+    private int numVoo;
+    private String horario;
 
-    // Constructor
-    public Passageiro(String CPF, String nome, String endereco, String telefone, int numPassagem,
-                      int numPoltrona, int numVoo, String horario) {
-        this.CPF = CPF;
-        this.Nome = nome;
-        this.Endereco = endereco;
-        this.Telefone = telefone;
-        this.NumPassagem = numPassagem;
-        this.NumPoltrona = numPoltrona;
-        this.NumVoo = numVoo;
-        this.Horario = horario;
+    public Passageiro(String cpf, String nome, String endereco, String telefone, int numPassagem, int numPoltrona, int numVoo, String horario) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.numPassagem = numPassagem;
+        this.numPoltrona = numPoltrona;
+        this.numVoo = numVoo;
+        this.horario = horario;
     }
 
-    public String getNome() {return Nome;}
-    public String getCPF() {return CPF;}
+    public int getNumPassagem() {return numPassagem;}
 
+    public int getNumPoltrona() {return numPoltrona;}
 
-    // toString
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 
     @Override
     public String toString() {
-        return "Passageiro" +
-                "CPF='" + CPF + '\'' +
-                ", Nome='" + Nome + '\'' +
-                ", Endereco='" + Endereco + '\'' +
-                ", Telefone=" + Telefone +
-                ", NumPassagem=" + NumPassagem +
-                ", NumPoltrona=" + NumPoltrona +
-                ", NumVoo=" + NumVoo +
-                ", Horario='" + Horario + "\n";
+        return "Passageiro{" +
+                "cpf='" + cpf + '\'' +
+                ", nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", numPassagem=" + numPassagem +
+                ", numPoltrona=" + numPoltrona +
+                ", numVoo=" + numVoo +
+                ", horario=" + horario +
+                '}';
     }
 }
